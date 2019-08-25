a9e2c8c5233846491163235e5853081077a6b164
Tommy Pham <phamtommylong1991@gmail.com>
2019-08-24 20:03:00 -0700

    port sniffer

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,21 @@ impl Arguments {
             if flag.contains("-h") || flag.contains("-help") && args.len() == 2 {
                 println!("Usage: -j to select how many threads you want
                 \r\n             -h or -help to show this help message");
-                return Err("help");
+                return Err("Help");
+            } else if flag.contains("-h") || flag.contains("-help") {
+                return Err("Too many arguments");
+            } else if flag.contains("-j") {
+                let ipaddr = match IpAddr::from_str(&args[3]) {
+                    Ok(s) => s,
+                    Err(_) => return Err("Not a valid IP address. Must be IPv4 or IPv6/")
+                };
+                let threads = match args[2].parse::<u16>() {
+                    Ok(s) => s,
+                    Err(_) => return Err("Failed to parse thread numberz")
+                };
+                return Ok(Arguments{threads, flag, ipaddr});
+            } else {
+                return Err("Invalid syntax");
             }
         }
     }
