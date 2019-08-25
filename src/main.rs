c4860387b662c4040a9dfbc468d6178af3347630
Tommy Pham <phamtommylong1991@gmail.com>
2019-08-24 19:52:35 -0700

    port sniffer

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::env;
+use std::net::IpAddr;
+
+struct Arguments {
+    flag: String,
+    ipaddr: IpAddr,
+    threads: u16,
+}
+
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone():
 
-    for i in &args {
-        println!("{}", i);
-    }
-
-    println!("{:?}", args);
 }
