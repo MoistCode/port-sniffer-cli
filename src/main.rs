aad9e9e797f78e3cb6828ae0f4a7516e57de9356
Tommy Pham <phamtommylong1991@gmail.com>
2019-08-24 19:56:16 -0700

    port sniffer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::net::IpAddr;
+use std::str::FromStr;
 
 struct Arguments {
     flag: String,
@@ -7,7 +8,15 @@ struct Arguments {
     threads: u16,
 }
 
-
+impl Arguments {
+    fn new(args: &[String]) -> Result<Arguments, &'static str> {
+        if args.len() < 2 {
+            return Err("Not enough arguments");
+        } else if args.len() > 4 {
+            return Err("Too many arguments");
+        }
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
